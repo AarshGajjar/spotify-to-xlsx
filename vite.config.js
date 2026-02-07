b1db278a79baea31c2d4f259649ea8bf2210ff3a
Aarsh <60134947+AarshGajjar@users.noreply.github.com>
2026-02-08 03:52:01 +0530

    fix: update base URL for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // For GitHub Pages compatibility
+  base: '/spotify-to-xlsx/', // For GitHub Pages compatibility
   server: {
     host: '127.0.0.1',
     port: 5173,
